e9d207d36ef7b2bb50106421edfabe3f8c352236
kylewong975 <kyle.wong975@gmail.com>
2018-02-17 09:13:22 -0800

    Reactstrap columns

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import InfoSection from './Components/InfoSection';
 import MicrophoneSection from './Components/MicrophoneSection';
 import AnnouncementsSection from './Components/AnnouncementsSection';
@@ -8,14 +9,20 @@ import MapSection from './Components/MapSection';
 export default class App extends Component {
   render() {
     return (
-      <div style={styles.overview}>
-        <h1>The Sound Guardian</h1>
-        <InfoSection schoolName="Stanford University" schoolAddress="450 Serra Mall, Stanford, CA 94305"/>
-        <MicrophoneSection />
-        <AnnouncementsSection />
-        <FormSection />
-        <MapSection />
-      </div>
+      <Container fluid style={styles.overview}>
+        <Row>
+          <Col>
+            <h1>The Sound Guardian</h1>
+            <InfoSection schoolName="Stanford University" schoolAddress="450 Serra Mall, Stanford, CA 94305"/>
+            <MicrophoneSection />
+            <AnnouncementsSection />
+          </Col>
+          <Col>
+            <FormSection />
+            <MapSection />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
