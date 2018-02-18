0295d5bb7aea962061397dc2c409d3f59948778b
HirdayGupta <hirday.gupta@gmail.com>
2018-02-18 02:03:43 -0800

    Make microphone magic

--- webapp/src/Components/MicrophoneSection.js
+++ webapp/src/Components/MicrophoneSection.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import Microphone from './Microphone';
+import base from '../rebase';
 
-export default class MicrophoneSection extends React.Component {
+class MicrophoneSection extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      microphones: [],
+      };
+  }
+  componentWillMount() {
+    /*
+     * We bind the 'chats' firebase endopint to our 'messages' state.
+     * Anytime the firebase updates, it will call 'setState' on this component
+     * with the new state.
+     *
+     * Any time we call 'setState' on our 'messages' state, it will
+     * updated the Firebase '/chats' endpoint. Firebase will then emit the changes,
+     * which causes our local instance (and any other instances) to update
+     * state to reflect those changes.
+     */
+    this.ref = base.syncState('microphones', {
+      context: this,
+      state: 'microphones',
+      asArray: true
+    });
+  }
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  renderMicrophone(mic) {
+    return (<Microphone name={mic.name}
+                        location={mic.location}
+                        condition={mic.condition}
+                        status={mic.status} />);
+  }
+
   render() {
     return(
       <div style={styles.overview}>
-        <Microphone name="Microphone 1" location="Hallway 1" condition="Active" status="SAFE" />
-        <Microphone name="Microphone 2" location="Corridor" condition="Active" status="SAFE" />
+          {this.state.microphones.map(this.renderMicrophone)}
       </div>
     )
   }
@@ -22,3 +57,5 @@ let styles = {
     height: 350,
   },
 }
+
+export default MicrophoneSection;
